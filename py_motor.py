ba45b36dab684c0823b281e4b3af9368e99db896
TaeHo Lee <67465736+Hooie@users.noreply.github.com>
2024-11-26 15:44:16 +0900

    프로젝트 1차 완료

--- py_motor.py
+++ py_motor.py
@@ -12,18 +12,17 @@ pwm.start(0.0)
 time.sleep(1)
 
 def TurnOn():
-	pwm.ChangeDutyCycle(9.5)
-	time.sleep(1.0)
+		pwm.ChangeDutyCycle(8.2)
+		time.sleep(1.0)
 
-def TurnOff():
-	pwm.ChangeDutyCycle(6.0)
-	time.sleep(1.0)
-	
+def TurnOff(): 
+		pwm.ChangeDutyCycle(6.0)
+		time.sleep(1.0)
+
 def Stop():
 	pwm.stop()
 	GPIO.cleanup()
 	
-#모터의 적정 duty값 체크
 def Checkmotor():
 	i = 1
 	while i:
@@ -32,3 +31,6 @@ def Checkmotor():
 		print(val)
 		i += 1
 		time.sleep(1.5)
+
+
+
